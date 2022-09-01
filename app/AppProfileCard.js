78dd51cca4b44d0db8f5f4bc1347f9dc9e8f1e50
evenvi <evenvi@163.com>
2022-09-01 10:35:54 +0800

    增加缩略图缩放

--- app/AppProfileCard.js
+++ app/AppProfileCard.js
@@ -1,29 +1,80 @@
 //胸卡
 import React, {Component} from 'react';
-import {Text, View, Button, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, Platform, TouchableHighlight} from 'react-native';
 import getStyleSheet from './styles';
+import PropTypes from 'prop-types';
+import update from 'immutability-helper';
+
+const userImage = require('./user.jpg');
+
+const data = [{
+  image: userImage,
+  name: '朱棣',
+  occupation: 'React Native Developer',
+  description: ' Zhudi is really great JavaScript developer. He\n' +
+    'loves using JS to build React Native Applications for iOS and Android',
+  showThumbnail: true,
+}];
+
+const ProfileCard = (props) => {
+  const {image, name, occupation, description, onPress, showThumbnail} = props;
+  let containerStyles = [styles.cardContainer];
+
+  if (showThumbnail) {
+    containerStyles.push(styles.cardThumbnail);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardContainer}>
+        <View style={styles.cardImageContainer}>
+          <Image source={require('./user.jpg')} style={styles.cardImage}/>
+        </View>
+        <View>
+          <Text style={styles.cardName}>{name}</Text>
+        </View>
+        <View style={styles.cardOccupationContainer}>
+          <Text style={styles.cardOccupation}>{occupation}</Text>
+        </View>
+        <Text style={styles.cardDescription}>{description}</Text>
+      </View>
+    </View>
+  );
+};
+
+ProfileCard.propTypes = {
+  image: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  occupation: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  showThumbnail: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
 
 export class AppProfileCard extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: data,
+    };
+  }
+
+  handleProfileCardPress = (index) => {
+    const showThumbnail = !this.state.data[index].showThumbnail;
+    this.setState({
+      data: update(this.state.data, {[index]: {showThumbnail: {$set: showThumbnail}}}),
+    });
+  };
+
   render() {
+    const list = this.state.data.map(function (item, index){
+      const {image, name, occupation, description, showThumbnail} = item;
+      return <ProfileCard key={'card-' + index} image={image} name={name} occupation={occupation} description={description} showThumbnail={showThumbnail} onPress={this.handleProfileCardPress.bind(this, index)}/>
+    }, this)
+
     return (
-      <View style={styles.container}>
-        <View style={styles.cardContainer}>
-          <View style={styles.cardImageContainer}>
-            <Image source={require('./user.jpg')} style={styles.cardImage}/>
-          </View>
-          <View>
-            <Text style={styles.cardName}>朱棣</Text>
-          </View>
-          <View style={styles.cardOccupationContainer}>
-            <Text style={styles.cardOccupation}>React native Developer</Text>
-          </View>
-          <Text style={styles.cardDescription}>
-            John is really great JavaScript developer. He
-            loves using JS to build React Native Applications for iOS and Android
-          </Text>
-        </View>
-      </View>
-    );
+      <view style={styles.container}>{list}</view>
+    )
   }
 }
 
@@ -36,6 +87,18 @@ const styles = StyleSheet.create({
   },
 
   cardContainer: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1,
+      },
+      android: {
+        elevation: 15,
+      },
+    }),
     alignItems: 'center',
     borderColor: 'black',
     borderWidth: 3,
@@ -47,6 +110,20 @@ const styles = StyleSheet.create({
   },
 
   cardImageContainer: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1,
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15,
+      },
+    }),
     alignItems: 'center',
     backgroundColor: 'white',
     borderWidth: 3,
@@ -64,8 +141,8 @@ const styles = StyleSheet.create({
     height: 112,
   },
 
-  cardName:{
-    color:'white',
+  cardName: {
+    color: 'white',
     fontWeight: 'bold',
     fontSize: 24,
     marginTop: 30,
@@ -81,11 +158,15 @@ const styles = StyleSheet.create({
   },
 
   cardDescription: {
-    fontStyle:'italic',
+    fontStyle: 'italic',
     marginTop: 10,
     marginRight: 40,
     marginLeft: 40,
     marginBottom: 10,
+  },
+
+  cardThumbnail: {
+    transform:[{scale: 0.2}]
   }
 });
 
